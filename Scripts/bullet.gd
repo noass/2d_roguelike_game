d2cbef2c1c6145b889047a79e7488a2127b1f3cd
noass <therealfunguyy@gmail.com>
2023-10-10 20:29:14 +0300

    added shooting with arrow keys, fixed smooth camera jitter

---
+++ Scripts/bullet.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+const SPEED = 500
+
+func _physics_process(delta):
+	position += transform.x * SPEED * delta
+
+func _on_body_entered(body):
+	if body.is_in_group("bulletStop"):
+		queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 const SPEED = 300
 
-func _physics_process(delta):
+var bullet = preload("res://Scenes/bullet.tscn")
+
+func _process(delta):
 	DisplayServer.window_set_title("2D roguelike | " + str(Engine.get_frames_per_second()) + " FPS")
 	var direction = Input.get_vector("A", "D", "W", "S")
 	if direction:
@@ -19,3 +21,25 @@ func _physics_process(delta):
 		$AnimationPlayer.play("idle")
 
 	move_and_slide()
+
+func _on_shoot_timer_timeout():
+	if Input.is_action_pressed("RIGHT"):
+		var bulletInstance = bullet.instantiate()
+		get_parent().add_child(bulletInstance)
+		bulletInstance.rotation_degrees = 0
+		bulletInstance.position = global_position
+	elif Input.is_action_pressed("LEFT"):
+		var bulletInstance = bullet.instantiate()
+		get_parent().add_child(bulletInstance)
+		bulletInstance.rotation_degrees = -180
+		bulletInstance.position = global_position
+	elif Input.is_action_pressed("UP"):
+		var bulletInstance = bullet.instantiate()
+		get_parent().add_child(bulletInstance)
+		bulletInstance.rotation_degrees = -90
+		bulletInstance.position = global_position
+	elif Input.is_action_pressed("DOWN"):
+		var bulletInstance = bullet.instantiate()
+		get_parent().add_child(bulletInstance)
+		bulletInstance.rotation_degrees = -270
+		bulletInstance.position = global_position
